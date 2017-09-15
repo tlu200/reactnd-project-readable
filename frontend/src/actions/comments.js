0520ea2c39500fe7787f1802abd79f0d31ff2c61
tlu200 <tlu200@users.noreply.github.com>
2017-09-15 01:46:33 -0700

    More post and comment actions

--- frontend/src/actions/comments.js
+++ frontend/src/actions/comments.js
@@ -1,8 +1,49 @@
 export const SET_COMMENTS = "SET_COMMENTS";
+export const ADD_COMMENT = "ADD_COMMENT";
+export const EDIT_COMMENT = "EDIT_COMMENT";
+export const DELETE_COMMENT = "DELETE_COMMENT";
+export const UP_VOTE_COMMENT = "UP_VOTE_COMMENT";
+export const DOWN_VOTE_COMMENT = "DOWN_VOTE_COMMENT";
 
 export function setComments (comments) {
   return {
     type: SET_COMMENTS,
     comments
+  }
+}
+
+export function addComment (post) {
+  return {
+    type: ADD_COMMENT,
+    post
+  }
+}
+
+export function editComment (commentId, changes) {
+  return {
+    type: EDIT_COMMENT,
+    commentId,
+    changes
+  }
+}
+
+export function deleteComment (commentId) {
+  return {
+    type: DELETE_COMMENT,
+    commentId
+  }
+}
+
+export function upVoteComment (commentId) {
+  return {
+    type: UP_VOTE_COMMENT,
+    commentId
+  }
+}
+
+export function downVoteComment (commentId) {
+  return {
+    type: DOWN_VOTE_COMMENT,
+    commentId
   }
 }
--- frontend/src/actions/posts.js
+++ frontend/src/actions/posts.js
@@ -1,8 +1,49 @@
 export const SET_POSTS = "SET_POSTS";
+export const ADD_POST = "ADD_POST";
+export const EDIT_POST = "EDIT_POST";
+export const DELETE_POST = "DELETE_POST";
+export const UP_VOTE_POST = "UP_VOTE_POST";
+export const DOWN_VOTE_POST = "DOWN_VOTE_POST";
 
 export function setPosts (posts) {
   return {
     type: SET_POSTS,
     posts
+  }
+}
+
+export function addPost (post) {
+  return {
+    type: ADD_POST,
+    post
+  }
+}
+
+export function editPost (id, changes) {
+  return {
+    type: EDIT_POST,
+    id,
+    changes
+  }
+}
+
+export function deletePost (id) {
+  return {
+    type: DELETE_POST,
+    id
+  }
+}
+
+export function upVotePost (id) {
+  return {
+    type: UP_VOTE_POST,
+    id
+  }
+}
+
+export function downVotePost (id) {
+  return {
+    type: DOWN_VOTE_POST,
+    id
   }
 }
