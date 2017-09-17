8a996719077c28db8d888546b3a69b9a66570c32
tlu200 <tlu200@users.noreply.github.com>
2017-09-16 23:23:00 -0700

    Post component

---
+++ frontend/src/components/Post.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Row, Panel, Badge } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+
+class Post extends Component {
+  static propTypes = {
+    post: PropTypes.object.isRequired,
+  };
+
+  render() {
+    const { post } = this.props;
+    return (
+      <Row>
+        <Panel header={post.title} bsStyle="primary">
+          {post.body}
+          <Badge pullRight="true">{post.voteScore}</Badge>
+        </Panel>
+      </Row>
+    );
+  }
+}
+
+export default Post;
