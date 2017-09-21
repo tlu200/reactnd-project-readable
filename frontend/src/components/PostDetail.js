d72f3df30ab5407c0674a0ee2fac19a7d92777f3
tlu200 <tlu200@users.noreply.github.com>
2017-09-20 22:30:55 -0700

    Delete and edit button in post detail page

--- frontend/src/components/PostDetail.js
+++ frontend/src/components/PostDetail.js
@@ -1,48 +1,101 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Grid, Row, Table } from 'react-bootstrap';
+import { Grid, Row, Table, ButtonGroup, Button } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { postsActions } from '../actions';
+import * as API from '../utils/api';
 
 const keys = ['id', 'timestamp', 'title', 'body', 'author', 'category', 'voteScore'];
 
-function PostDetail (props) {
-  const post = props.posts.find((post) => {
-    return post.id === props.match.params.postId;
-  });
+class PostDetail extends Component {
+  constructor(props) {
+    super(props);
+    const { posts, match } = this.props;
+    const post = posts.find((post) => {
+      return post.id === match.params.postId;
+    });
+    this.state = {
+      post: post
+    }
+  }
 
-  return (
-    <Grid>
-      <Row>
+  componentDidMount () {
+    const { match } = this.props;
+    const postId = match.params.postId;
+    if(!this.state.post) {
+      API.getPost(postId).then((response) => {
+        if(response.id) {
+          this.setState({ post: response });
+        }
+      })
+    }
+  }
+
+  deletePost() {
+    const { match, deletePost, history } = this.props;
+    const postId = match.params.postId;
+    if(postId) {
+      API.deletePost(postId).then((post) => {
+        deletePost(post.id);
+        history.push("/");
+      });
+    }
+  }
+
+  render() {
+    const { post } = this.state;
+
+    return (
+      <Grid>
         {
           post ?
-            <Table>
-              <thead>
-              <tr>
-                <th>#</th>
-                <th>value</th>
-              </tr>
-              </thead>
-              <tbody>
-              {keys.map((key) => {
-                return (
-                  <tr key={key}>
-                    <td>{key}</td>
-                    <td>{key === 'timestamp'? new Date(post[key]).toISOString(): post[key]}</td>
-                  </tr>
-                );
-              })}
-              </tbody>
-            </Table> : <div>No post found</div>
+            <Row>
+              <Table>
+                <thead>
+                <tr>
+                  <th>#</th>
+                  <th>value</th>
+                </tr>
+                </thead>
+                <tbody>
+                {keys.map((key) => {
+                  return (
+                    <tr key={key}>
+                      <td>{key}</td>
+                      <td>{key === 'timestamp'? new Date(post[key]).toISOString(): post[key]}</td>
+                    </tr>
+                  );
+                })}
+                </tbody>
+              </Table>
+              <ButtonGroup>
+                <LinkContainer to={`/edit/${post.id}`}>
+                  <Button bsStyle="primary">Edit</Button>
+                </LinkContainer>
+                <Button bsStyle="primary" onClick={() => this.deletePost()}>Delete</Button>
+              </ButtonGroup>
+            </Row> :
+            <Row>
+              No post found
+            </Row>
         }
-      </Row>
-    </Grid>
-  );
+      </Grid>
+    );
+  }
 }
 
 function mapStateToProps ({ posts }) {
   return { posts };
 }
 
+function mapDispatchToProps (dispatch) {
+  return {
+    deletePost: (id) => dispatch(postsActions.deletePost(id)),
+  }
+}
+
 
 export default connect(
-  mapStateToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(PostDetail);
