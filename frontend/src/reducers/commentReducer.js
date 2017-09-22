4803159fac12356659faa9f6cfc4827617364b9f
tlu200 <tlu200@users.noreply.github.com>
2017-09-22 00:31:27 -0700

    Fix commentReducer

--- frontend/src/reducers/commentReducer.js
+++ frontend/src/reducers/commentReducer.js
@@ -16,7 +16,7 @@ function editComment (state, action) {
 }
 
 function deleteComment (state, action) {
-  return state.filter((comment) => comment.commentId !== action.id);
+  return state.filter((comment) => comment.id !== action.commentId);
 }
 
 function upVoteComment (state, action) {
